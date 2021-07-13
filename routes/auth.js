834e6e26a57b1f450762074028a72f54e6f52118
Tanoluchi <lucianox15867@gmail.com>
2021-07-13 18:23:23 -0300

    Creando Token

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const Usuario = require('../models/usuario_model.js');
 const ruta = express.Router();
@@ -7,9 +8,21 @@ ruta.post('/', (req, res) => {
 	Usuario.findOne({email: req.body.email})
 		.then(datos => {
 			if(datos) {
+				// Validacion de contraseña
 				const passwordValido = bcrypt.compareSync(req.body.password, datos.password);
 				if(!passwordValido) return res.status(400).json({error:'ok', msj: 'Usuario o contraseña incorrecta'});
-				res.json(datos);
+				// Creación del token
+				const jwToken = jwt.sign({
+					data: {_id: datos._id, nombre: datos.name, email: datos.email}
+				}, 'secret', {expiresIn: '1h'});
+				res.json({
+					usuario: {
+						_id: datos._id,
+						nombre: datos.name,
+						email: datos.email
+					},
+					token: jwToken
+				});
 			}else{
 				res.status(400).json({
 					error: 'ok',
